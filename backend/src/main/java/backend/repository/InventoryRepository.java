46d3af6337425e5e0c3634028ec4acd9d4d15e25
tharindu9216 <tharindukaveeshaperera9216@gmail.com>
2025-04-06 18:15:13 +0530

    rechande the frotend page

--- backend/src/main/java/backend/repository/InventoryRepository.java
+++ backend/src/main/java/backend/repository/InventoryRepository.java
@@ -2,7 +2,7 @@ package backend.repository;
 
 import backend.model.InventoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository; // This import is not used
 
 public interface InventoryRepository extends JpaRepository<InventoryModel, Long> {
 
